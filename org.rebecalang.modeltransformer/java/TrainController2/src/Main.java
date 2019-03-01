82dffa3f4e51936a8afdba04817fe13727b0ee10
Fujitsu <Fujitsu@Fujitsu-PC>
2019-03-01 20:47:39 +0330

    1397/12/10_ priority queue management

--- org.rebecalang.modeltransformer/java/TrainController2/src/Main.java
+++ org.rebecalang.modeltransformer/java/TrainController2/src/Main.java
@@ -1,8 +1,34 @@
 public class Main{
 public static void main(String[] args){
+Message a = new Message();
+a = MessageQueue.getMessageQueue().poll();
 Train train1 = new Train("train1");
 Train train2 = new Train("train2");
 BridgeController theController = new BridgeController("theController");
+if (a.getReceiver().equals("theController") && a.getMsgName().equals("Arrive")) {
+	theController.Arrive;
+}
+if (a.getReceiver().equals("theController") && a.getMsgName().equals("Leave")) {
+	theController.Leave;
+}
+if (a.getReceiver().equals("train1") && a.getMsgName().equals("YouMayPass")) {
+	train1.YouMayPass;
+}
+if (a.getReceiver().equals("train2") && a.getMsgName().equals("YouMayPass")) {
+	train2.YouMayPass;
+}
+if (a.getReceiver().equals("train1") && a.getMsgName().equals("Passed")) {
+	train1.Passed;
+}
+if (a.getReceiver().equals("train2") && a.getMsgName().equals("Passed")) {
+	train2.Passed;
+}
+if (a.getReceiver().equals("train1") && a.getMsgName().equals("ReachBridge")) {
+	train1.ReachBridge;
+}
+if (a.getReceiver().equals("train2") && a.getMsgName().equals("ReachBridge")) {
+	train2.ReachBridge;
+}
 	
 }
 }
--- org.rebecalang.modeltransformer/src/main/java/org/rebecalang/modeltransformer/java/timedrebeca/MainFileCreator.java
+++ org.rebecalang.modeltransformer/src/main/java/org/rebecalang/modeltransformer/java/timedrebeca/MainFileCreator.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import org.apache.maven.artifact.repository.metadata.Metadata;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.MainRebecDefinition;
+import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.MsgsrvDeclaration;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.OrdinaryPrimitiveType;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.ReactiveClassDeclaration;
 import org.rebecalang.compiler.modelcompiler.corerebeca.objectmodel.RebecaModel;
@@ -30,28 +31,55 @@ public class MainFileCreator {
 	public String getMainFileContent() {
 		// TODO Auto-generated method stub
 		String retValue = "";
-		retValue += "public class Main{\r\n" + 
-				"public static void main(String[] args){\r\n" + 
-				"Message a = new Message();\r\n"+
-				defineRebecs() +
-				"	\r\n" + 
-				"}\r\n" + 
-				"}";
+		retValue += "public class Main{\r\n" + "public static void main(String[] args){\r\n"
+				+ "Message a = new Message();\r\n" + "a = MessageQueue.getMessageQueue().poll();\r\n" + defineRebecs()
+				+ queueManagement() + "	\r\n" + "}\r\n" + "}";
+		return retValue;
+	}
+
+	public String queueManagement() {
+		// TODO Auto-generated method stub
+		String retValue = "";
+		for (ReactiveClassDeclaration rc : rebecaModel.getRebecaCode().getReactiveClassDeclaration()) {
+			for (MsgsrvDeclaration msgsrv : rc.getMsgsrvs()) {
+				for (MainRebecDefinition md : rebecaModel.getRebecaCode().getMainDeclaration()
+						.getMainRebecDefinition()) {
+					ReactiveClassDeclaration metaData;
+					try {
+						metaData = TypesUtilities.getInstance().getMetaData(md.getType());
+						if (rc.getName() == metaData.getName()) {
+							retValue += "if (a.getReceiver().equals(\"" + md.getName()
+									+ "\") && a.getMsgName().equals(\"" + msgsrv.getName() + "\")) {\r\n\t" + md.getName()
+									+ "." + msgsrv.getName() + ";\r\n" + "}\r\n";
+
+							System.out.println(rc.getName() + "&&&" + msgsrv.getName() + "&&&" + metaData.getName()
+									+ "&&&" + md.getName());
+						}
+
+					} catch (CodeCompilationException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+			}
+		}
 		return retValue;
 	}
 
 	private String defineRebecs() {
 		// TODO Auto-generated method stub
 		String retValue = "";
-		for(MainRebecDefinition md: rebecaModel.getRebecaCode().getMainDeclaration().getMainRebecDefinition()) {
+		for (MainRebecDefinition md : rebecaModel.getRebecaCode().getMainDeclaration().getMainRebecDefinition()) {
 			try {
 				ReactiveClassDeclaration metaData = TypesUtilities.getInstance().getMetaData(md.getType());
-				retValue +=  metaData.getName() + " " + md.getName() + " = new " + metaData.getName() + "(\"" + md.getName() + "\""  + ");" + NEW_LINE;
+				retValue += metaData.getName() + " " + md.getName() + " = new " + metaData.getName() + "(\""
+						+ md.getName() + "\"" + ");" + NEW_LINE;
 			} catch (CodeCompilationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 		return retValue;
 	}
