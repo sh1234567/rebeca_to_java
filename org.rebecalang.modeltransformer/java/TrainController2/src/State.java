952bf861294241af9ed0154491c1098d3f94cb08
Fujitsu <Fujitsu@Fujitsu-PC>
2019-03-12 13:31:15 +0330

    1397/12/21- Check the equality of two arrays of messages

--- org.rebecalang.modeltransformer/java/TrainController2/src/State.java
+++ org.rebecalang.modeltransformer/java/TrainController2/src/State.java
@@ -6,5 +6,31 @@ public class State implements Cloneable {
 
 	public Object clone() throws CloneNotSupportedException {
 		return super.clone();
-	}
+public boolean equalMessagesArray(Message[] m1, Message[] m2) {
+		int n = 0;
+		int m = 0;
+		for (int i = 0; i < m1.length; i++)
+			if (m1[i] != null)
+				n++;
+		for (int i = 0; i < m2.length; i++)
+			if (m2[i] != null)
+				m++;
+		if (n != m)
+			return false;
+		int a[] = new int[n];
+		int b[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; i < n; i++) {
+				if (m1[i].equals(m2[j]) && b[j]!=1) {
+					a[i] = 1;
+					b[j]=1;
+				}
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			if (a[i] == 0)
+				return false;
+		}
+		return true;
+	}	}
 }
--- org.rebecalang.modeltransformer/src/main/java/org/rebecalang/modeltransformer/java/timedrebeca/StateFileCreator.java
+++ org.rebecalang.modeltransformer/src/main/java/org/rebecalang/modeltransformer/java/timedrebeca/StateFileCreator.java
@@ -15,6 +15,33 @@ public class StateFileCreator {
 				"\r\n" + 
 				"	public Object clone() throws CloneNotSupportedException {\r\n" + 
 				"		return super.clone();\r\n" + 
+				"public boolean equalMessagesArray(Message[] m1, Message[] m2) {\r\n" + 
+				"		int n = 0;\r\n" + 
+				"		int m = 0;\r\n" + 
+				"		for (int i = 0; i < m1.length; i++)\r\n" + 
+				"			if (m1[i] != null)\r\n" + 
+				"				n++;\r\n" + 
+				"		for (int i = 0; i < m2.length; i++)\r\n" + 
+				"			if (m2[i] != null)\r\n" + 
+				"				m++;\r\n" + 
+				"		if (n != m)\r\n" + 
+				"			return false;\r\n" + 
+				"		int a[] = new int[n];\r\n" + 
+				"		int b[] = new int[n];\r\n" + 
+				"		for (int i = 0; i < n; i++) {\r\n" + 
+				"			for (int j = 0; i < n; i++) {\r\n" + 
+				"				if (m1[i].equals(m2[j]) && b[j]!=1) {\r\n" + 
+				"					a[i] = 1;\r\n" + 
+				"					b[j]=1;\r\n" + 
+				"				}\r\n" + 
+				"			}\r\n" + 
+				"		}\r\n" + 
+				"		for (int i = 0; i < n; i++) {\r\n" + 
+				"			if (a[i] == 0)\r\n" + 
+				"				return false;\r\n" + 
+				"		}\r\n" + 
+				"		return true;\r\n" + 
+				"	}" +
 				"	}\r\n" + 
 				"}\r\n" + 
 				"";
